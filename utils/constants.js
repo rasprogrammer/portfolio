6f76fa314c8614b99d486a55717adcf049ea4cf4
ras <rasprogrammer@gmail.com>
2026-07-16 08:25:54 +0530

    resume updated and social media icons fixed

--- utils/constants.js
+++ utils/constants.js
@@ -5,6 +5,6 @@ export const SITE_LINKS = {
   linkedin: "https://www.linkedin.com/in/rajeev-kumar-2b9767319/",
   email: "rasprogrammer@gmail.com",
   whatsapp: "https://wa.me/+916202784972",
-  resume: "https://drive.google.com/file/d/1V5HIpGx9uftqxF_eOPY2ZUFKmTkB4Nn_/view"
+  resume: "https://drive.google.com/file/d/1V5HIpGx9uftqxF_eOPY2ZUFKmTkB4Nn_/view?usp=drive_link"
 };
 
